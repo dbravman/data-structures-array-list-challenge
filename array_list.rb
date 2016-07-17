7d9d1427ebcbb7da0deddf032d5d2917285f7de5
Danny Bravman <dbravman@gmail.com>
2016-07-17 16:40:53 -0500

    methods per spec

--- array_list.rb
+++ array_list.rb
@@ -6,6 +6,20 @@ class ArrayList
   end
 
   def add(element)
-    @array = FixedArray.new()
+    @array = FixedArray.new(@array.length+1)
+    @array.set(@array.length-1, element)
+  end
+
+  def get(index)
+    @array.get(index)
+  end
+
+  def set(index, element)
+    raise "OutOfBoundsException" if @array.get(index) == nil
+    @array.set(index, element)
+  end
+
+  def length
+    @array.length
   end
 end
--- array_list_spec.rb
+++ array_list_spec.rb
@@ -1,5 +1,23 @@
 require_relative 'array_list'
 
 describe ArrayList do
+  let(:array_list) { ArrayList.new(3) }
 
+  it "has add" do
+    expect(array_list.add("test")).to eq("test")
+    expect(array_list.length).to eq(4)
+  end
+
+  it "has get" do
+    expect {array_list.get(3)}.to raise_error("OutOfBoundsException")
+    array_list.add("test")
+    expect(array_list.get(3)).to eq("test")
+  end
+
+  it "has set" do
+    expect {array_list.set(0, "test")}.to raise_error("OutOfBoundsException")
+    expect {array_list.set(3, "test")}.to raise_error("OutOfBoundsException")
+    
+    expect {array_list.set(2, "test")}.to raise_error("OutOfBoundsException")
+  end
 end
